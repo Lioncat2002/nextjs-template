55ec6d82c4183e18f551d9d84108f876ae56f5e6
Tweedle2Dum <utkarsh.work1405@gmail.com>
2025-07-20 15:19:24 +0530

    add env config

---
+++ app/api/auth/login/route.ts
@@ -0,0 +1,53 @@
+import { createSessionManager } from "@/src/shared/auth/session";
+import {
+	ERROR_MESSAGES,
+	StatusCodes,
+} from "@/src/shared/constants/status-code-messages";
+import { type NextRequest, NextResponse } from "next/server";
+
+export const runtime = "edge";
+
+type LoginResponse = {
+	data: {
+		access_token: string;
+		refresh_token: string | null;
+	};
+};
+
+export async function POST(req: NextRequest) {
+	console.info("Login request received");
+	try {
+		const { idToken } = await req.json();
+		if (!idToken) {
+			console.error("Missing idToken");
+			return NextResponse.json(
+				{ ok: false, message: "Missing idToken" },
+				{ status: 400 },
+			);
+		}
+		console.info("idToken received");
+		const { setSession } = createSessionManager();
+		console.info("Session manager created");
+
+		/* TODO: Implement login logic -> ID TOKEN, IF USER EXISTS IN DB LOGIN, ELSE CREATE NEW */
+
+		console.info("Login response received");
+		await setSession({
+			accessToken: "",
+			refreshToken: "",
+		});
+		console.info("Session set");
+
+		return NextResponse.json({
+			ok: true,
+			message: "Login successful",
+			data: null,
+		});
+	} catch (err) {
+		console.error("Login error:", err);
+		return NextResponse.json(
+			{ ok: false, message: ERROR_MESSAGES[500], data: null },
+			{ status: StatusCodes.HTTP_500_INTERNAL_SERVER_ERROR },
+		);
+	}
+}
--- src/config/index.ts
+++ src/config/index.ts
@@ -10,7 +10,7 @@ export const env = createEnv({
 		/* SERVER SIDE ENVIRONMENT VARIABLES */
 		DATABASE_URL: z.string().url(),
 		NODE_ENV: z.enum(["development", "production", "test"]),
-		SECRET_COOKIE_PASSWORD: z.string(),
+		SECRET_COOKIE_PASSWORD: z.string().min(32),
 	},
 	/*
 	 * Environment variables available on the client (and server).
@@ -20,6 +20,13 @@ export const env = createEnv({
 	client: {
 		/* CLIENT SIDE ENVIRONMENT VARIABLES */
 		NEXT_PUBLIC_API_URL: z.string().url(),
+		NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
+		NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string(),
+		NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string(),
+		NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string(),
+		NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z.string(),
+		NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: z.string(),
+		NEXT_PUBLIC_FIREBASE_APP_ID: z.string(),
 	},
 	/*
 	 * Specify what values should be validated by your schemas above.
@@ -33,6 +40,18 @@ export const env = createEnv({
 		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
 		NODE_ENV: process.env.NODE_ENV,
 		SECRET_COOKIE_PASSWORD: process.env.SECRET_COOKIE_PASSWORD,
+		NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+		NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:
+			process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+		NEXT_PUBLIC_FIREBASE_PROJECT_ID:
+			process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+		NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET:
+			process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+		NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID:
+			process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+		NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID:
+			process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+		NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 	},
 	// experimental__runtimeEnv: {
 	//   NEXT_PUBLIC_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_PUBLISHABLE_KEY,
